74ec540e89ea078bc3255262577cf38f3da9f734
DanielHauge <Animcuil@gmail.com>
2017-10-12 21:50:10 +0200

    Added UFOCSHARPS

---
+++ Ressources/UFO/Meassurer/WE/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+
+namespace Measure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://139.59.132.185:8080");
+
+            Stopwatch timer = new Stopwatch();
+
+            timer.Start();
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            timer.Stop();
+
+            TimeSpan timeTaken = timer.Elapsed;
+            Console.WriteLine("Seconds: "+timeTaken.Seconds+" Miliseconds: "+timeTaken.Milliseconds);
+            Console.WriteLine("Full time taken: "+timeTaken);
+            Console.ReadKey();
+        }
+    }
+}
